cf889512db18c511e6c854814823acf69b488177
Ricardo-Cacheira <34067691+Ricardo-Cacheira@users.noreply.github.com>
2021-07-20 16:43:22 +0100

    Initial commit
    Temporary text but most of the structure is in place

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+var links = document.links;
+for (var i = 0; i < links.length; i++) {
+    links[i].target = "_blank";
+}
+
+const base = {
+    origin: 'bottom',
+    delay: 300,
+    distance: '120px',
+    easing: 'ease-in-out',
+};
+
+const project = {
+    origin: 'left',
+    delay: 200,
+    distance: '200px',
+    easing: 'ease-in-out',
+};
+
+const award = {
+    origin: 'left',
+    delay: 300,
+    interval: 300,
+    distance: '200px',
+    easing: 'ease-in-out',
+};
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animate', base);
+sr.reveal('.project', project);
+sr.reveal('.award', award);
