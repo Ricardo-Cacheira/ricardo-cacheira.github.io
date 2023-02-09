c4489ae320b030901cdafee8270b1f8528ee8882
Ricardo Cacheira <ricardo.cacheira@hotmail.com>
2023-02-09 12:17:35 +0000

    Published games pages

--- assets/main.js
+++ assets/main.js
@@ -39,64 +39,83 @@ sr.reveal('.project', project);
 sr.reveal('.award', award);
 sr.reveal('.hr3', hr);
 
-// var thisis = document.getElementById("wrapper");
-// var tyty = document.getElementById("move");
-// var witth = tyty.offsetWidth;
+const html = document.querySelector('html');
+html.style.scrollbarWidth = 'thin';
 
+var currentProject;
 
+function openProject(index) {
+    currentProject = index;
+    var element = document.getElementById("project"+index);
+    var fade = document.getElementById("fade");
+    element.classList.add("open");
+    element.classList.remove("hidden");
+    fade.classList.add("fadeAnim");
+    fade.classList.remove("hidden");
+    html.style.scrollbarWidth = 'none';
+    disableScroll();
+}
 
-// thisis.style.paddingRight = witth + "px";
-
-
-const track = document.getElementById("image-track");
-
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
-
-const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
-};
-
-const handleOnMove = e => {
-    if (track.dataset.mouseDownAt === "0") return;
-
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-    maxDelta = window.innerWidth / 2;
-
-    const percentage = mouseDelta / maxDelta * -100,
-    nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-
-    track.dataset.percentage = nextPercentage;
-
-    track.animate({
-    transform: `translate(${nextPercentage}%, -50%)`
-    }, {
-    duration: 1200,
-    fill: "forwards"
-    });
-
-    for (const image of track.getElementsByClassName("image")) {
-    image.animate({
-        objectPosition: `${100 + nextPercentage}% center`
-    }, {
-        duration: 1200,
-        fill: "forwards"
-    });
+function closeProject() {
+    var element = document.getElementById("project"+currentProject);
+    var fade = document.getElementById("fade");
+    element.classList.add("hidden");
+    element.classList.remove("open");
+    fade.classList.add("hidden");
+    fade.classList.remove("fadeAnim");
+    html.style.scrollbarWidth = 'thin';
+    enableScroll();
+    let iframe_tag = element.querySelector( 'iframe');
+    let video_tag = element.querySelector( 'video' );
+    if ( iframe_tag) {
+        let iframeSrc = iframe_tag.src;
+        iframe_tag.src = iframeSrc; 
     }
-};
+    if ( video_tag) {
+        video_tag.pause();
+    }
+}
 
-/* -- Had to add extra lines for touch events -- */
+//scroll controll
 
-window.onmousedown = e => handleOnDown(e);
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
-window.ontouchstart = e => handleOnDown(e.touches[0]);
+function preventDefault(e) {
+  e.preventDefault();
+}
 
-window.onmouseup = e => handleOnUp(e);
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
 
-window.ontouchend = e => handleOnUp(e.touches[0]);
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
 
-window.onmousemove = e => handleOnMove(e);
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
 
-window.ontouchmove = e => handleOnMove(e.touches[0]);
-//# sourceURL=pen.js
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
